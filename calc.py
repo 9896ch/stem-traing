3bec4d44ad6f826543fdff3154b1af6b07b78e05
9896ch <hazymine820@gmail.com>
2022-05-19 02:28:07 -0700

    strings in python

---
+++ calc.py
@@ -0,0 +1,5 @@
+f_num = input ("enter first number")
+s_num = input ("enter second number")
+
+sum_of_num = f_num +s_num
+print("The sum of the two number is") + str(sum_of_num)
---
+++ strings.py
@@ -0,0 +1,40 @@
+name="Derrick Munyeki" 
+
+# Multiple line strings
+msg ="""QRS3X6 confirmed you have received ksh 1200 from Janet Mueni safaricom cares for you"""
+
+
+
+
+
+
+
+
+
+city = "nairobi"
+# .upper() to convert to upper case
+print(city.upper())
+
+
+uni="JKUAT"
+print(uni.lower())
+
+fruit = "strawberry"
+print(fruit[:2])
+print(fruit[:5])
+print(fruit[-3:])
+
+#strip()remove spaces before thr words
+f_name =" Bob"
+print(f_name.strip())
+
+f_name="derrick"
+s_name="munyeki"
+full_name=f_name+s_name
+print=(full_name)
+city="london"
+print(len(city))
+
+
+word = "deoxyribonucleicacid"
+print(len(word))
